1fd275e6a3fb03852d6c3ed5d0e03c79ae00f9d3
talhatariqc232 <70269153+talhatariqc232@users.noreply.github.com>
2020-08-26 07:22:31 -0700

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Please enter your name",
+                    minlength: "Name incomplete"
+                },
+                subject: {
+                    required: "Please enter the subject",
+                    minlength: "Your subject must consist of at least 4 characters"
+                },
+                number: {
+                    required: "Please enter your number",
+                    minlength: "Your number must consist of at least 5 characters"
+                },
+                email: {
+                    required: "Please enter your email"
+                },
+                message: {
+                    required: "Please enter your message",
+                    minlength: "Please write your complete message"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
